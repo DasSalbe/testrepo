c6fef17bc94a4a94a551e0bff4e7f994066c2a4e
DasSalbe <s777n.o.negative@gmail.com>
2026-06-14 04:17:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-document.getElementById("startBtn")
-.addEventListener("click", function() {
-
+function showHome() {
     document.body.innerHTML = `
-        <h1>Will you officially be my girlfriend? ❤️</h1>
+        <div class="container">
+            <h1>A digital kinda gift to my dearest, Angel.</h1>
 
-        <button onclick="yesClicked()">Yes</button>
-
-        <button id="noBtn">No</button>
+            <button id="startBtn">Open</button>
+        </div>
     `;
 
-    const noBtn = document.getElementById("noBtn");
+    document
+        .getElementById("startBtn")
+        .addEventListener("click", showLetter);
+}
 
-    noBtn.addEventListener("mouseover", () => {
-        noBtn.style.position = "absolute";
-        noBtn.style.left = Math.random() * 80 + "%";
-        noBtn.style.top = Math.random() * 80 + "%";
-    });
-});
-
-function yesClicked() {
+function showLetter() {
     document.body.innerHTML = `
-        <h1>You just made me the happiest guy alive ❤️</h1>
+        <button onclick="showHome()" class="backBtn">←</button>
+
+        <div class="content">
+
+            <div class="left-column">
+
+                <h1>Injeell</h1>
+
+                <p>
+                    To my dearest, Angel,
+                    <br><br>
+
+                    This website is something I made last night, when you fell asleep, especially just for you.
+                    I wanted to create something that would remind you of
+                    how much you mean to me, and along the way, ask you on something serious.
+
+                    <br><br>
+
+                    Insert your letter here.
+                    Make it as long as you want.
+                    The text will stay on the left side of the screen.
+                </p>
+
+            </div>
+
+            <div class="right-column">
+
+                <audio controls>
+                    <source src="music.mp3" type="audio/mpeg">
+                </audio>
+
+                <div class="heart">
+                    <div class="heart-text">
+                        Talking Since
+                        <br><br>
+
+                        June 14, 2025
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     `;
 }
+
+showHome()
+showLetter1()
+showLetter2()
+showMemories()
+showHeartPage()
+showQuestion()
+showYesPage()
+showNoPage();
